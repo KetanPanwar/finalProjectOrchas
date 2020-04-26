b154c1eaeb480bdbc55f43f843ca214925e81157
KetanPanwar99 <ketanpanwar99@gmail.com>
2020-04-26 16:21:10 +0530

    fixing scaling

--- Orchas/orchas.py
+++ Orchas/orchas.py
@@ -121,16 +121,16 @@ def updateinfo():
 		container_id = i.id 
 		container_name = i.name
 		if 'slavespaw' in container_name:
-			# stream = os.popen( "sudo docker inspect --format '{{.State.Pid}}'" +'"'+ str(container_id)+'"') 
-			# container_pid = stream.read()
+			stream = os.popen( "sudo docker inspect --format '{{.State.Pid}}'" +'"'+ str(container_id)+'"') 
+			container_pid = stream.read()
 			print("yes")
 			# cm='GET /v1.24/containers/'+container_id+'/json?size=1 HTTP/1.1'
 			# resp_send = requests.get(
 			# 	cm, )
 			# d = json.loads(resp_send.content)
 			# d=json.loads(d)
-			print("cid",d)
-			# container_pid = int(container_pid) 
+			print("cid",)
+			container_pid = int(container_pid) 
 			running_containers_info.append( [str(container_pid),str(container_id),str(container_name)])
 	running_containers_info.sort()
 
